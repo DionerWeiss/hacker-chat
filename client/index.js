a4a670243c03c924d7701ccd0d481e3493bb1c5a
DionerWeiss <dionerweiss@gmail.com>
2021-03-24 23:11:22 -0300

    server real time connection with socket http protocol

--- client/index.js
+++ client/index.js
@@ -1,5 +1,13 @@
+
+
 import Events from 'events'
+import CliConfig from './src/cliConfig.js';
 import TerminalController from "./src/terminalController.js";
+
+const [nodePath, filePath, ...commands] = process.argv
+
+const config = CliConfig.parseArguments(commands)
+console.log(config);
 
 const componentEmitter = new Events()
 
